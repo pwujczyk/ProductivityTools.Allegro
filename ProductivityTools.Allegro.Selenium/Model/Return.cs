964b2d203a0f5f04cbc557c1957d65d5f748efad
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-08-07 00:14:33 +0200

    return

---
+++ ProductivityTools.Allegro.Selenium/Model/Return.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Allegro.Selenium.Model
+{
+    class Return
+    {
+        public string Id { get; set; }
+        public List<PurchaseItem> Items { get; set; }
+    }
+}
